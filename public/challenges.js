6d2e1c2566d476b8fbc60c95b0ebb1d73a76a963
Kah Kit Zheng <kahkitzheng@gmail.com>
2022-10-05 15:46:17 +0200

    Add Github page

---
+++ public/challenges.js
@@ -0,0 +1,37 @@
+export const challenges = [
+  {
+    name: "stats-preview",
+    title: "Stats preview card component",
+    level: "Newbie",
+    internalhost: true,
+    id: 1,
+  },
+  {
+    name: "3-column-preview-card",
+    title: "Three-column preview card component",
+    level: "Newbie",
+    internalhost: true,
+    id: 2,
+  },
+  {
+    name: "profile-card",
+    title: "Profile card component",
+    level: "Newbie",
+    internalhost: true,
+    id: 3,
+  },
+  {
+    name: "qr-code-component",
+    title: "Stats preview card component",
+    level: "Newbie",
+    internalhost: true,
+    id: 4,
+  },
+  {
+    name: "sunnyside-agency-landing-page",
+    title: "Sunnyside agency landing page",
+    level: "Junior",
+    internalhost: true,
+    id: 5,
+  },
+];
---
+++ public/script.js
@@ -0,0 +1,37 @@
+import { challenges } from "./challenges.js";
+
+loadData();
+
+function loadData() {
+  challenges.forEach((item) => {
+    createCard(item);
+  });
+}
+
+function createCard(item) {
+  const gridEl = document.getElementById("gridEl");
+  const url = item.internalhost === true ? `./${item.name}` : item.url;
+  const image = `./${item.name}/design/desktop-solution.png`;
+  const repo =
+    item.internalhost === true
+      ? `https://github.com/KahKitZheng/frontend-mentor/tree/main/${item.name}`
+      : item.repo;
+
+  const cardHTML = `
+    <li class="card">
+      <div class="image">
+        <img src=${image} alt=""/>
+      </div>
+      <div class="text">            
+        <h2>
+          <a href="${url}">${item.title}</a>
+        </h2>          
+        <small>
+          <a href="${repo}">Github repo</a>
+        </small>  
+      </div>
+    </li>
+  `;
+
+  gridEl.insertAdjacentHTML("beforeend", cardHTML);
+}
